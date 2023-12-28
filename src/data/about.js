bf349d2d806b126d4534b31d23c40f660c3bf98f
Arunachalam <arun@gogosoon.com>
2023-12-28 22:19:02 +0530

    [Arunachalam] Updates designation and description

--- src/data/about.js
+++ src/data/about.js
@@ -3,9 +3,9 @@ import Strings from "@/constants/strings";
 const About = {
   welcomeText: "Hi 👋 I'm",
   fullName: Strings.fullName,
-  designation: "Software Enginner & Fullstack Developer",
+  designation: "Senior Technologist",
   description:
-    "Welcome to my portfolio! I'm a passionate full stack developer dedicated to creating and designing user-centric software that prioritizes ease of use. With proficiency in both front-end and back-end development across various platforms, I specialize in seamlessly integrating cutting-edge technologies while excelling in design.",
+    "I'm a Techie with 3+ years of experience in Enterprise software development. Curious to explore new technologies, and domains and make an impact with the technology. I'll be leading the team, doing code reviews, conducting sessions, engage with clients, and obviously write code whenever my team wants me to jump in. ",
   followText: "Follow me here",
   avatarUrl: "/images/profile.webp",
 };
