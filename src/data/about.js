f8444eeea06cb78e09484b2a9310a0b5fcb90656
Arunachalam <arun@gogosoon.com>
2023-12-29 13:45:37 +0530

    [Arunachalam] Changes Profile photo

--- src/data/about.js
+++ src/data/about.js
@@ -7,7 +7,7 @@ const About = {
   description:
     "I'm a Techie with 3+ years of experience in Enterprise software development. Curious to explore new technologies, and domains and make an impact with the technology. I'll be leading the team, doing code reviews, conducting sessions, engage with clients, and obviously write code whenever my team wants me to jump in. ",
   followText: "Follow me here",
-  avatarUrl: "/images/profile.webp",
+  avatarUrl: "/images/Arunachalam.jpg",
 };
 
 export default About;
