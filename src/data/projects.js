729e6f06a07b91b7c23444dd3785224dfd31aaa7
Arunachalam <arun@gogosoon.com>
2023-12-29 15:25:38 +0530

    [Arunachalam] Removes unnecessary repository prefix

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ const projects = [
     title: "HOA Messenger",
     description:
       "An integrated SaaS platform designed for efficient communication and collaboration among HOA managers, residents, and vendors.",
-    icon: "/Arunachalam_Portfolio/logo/HOA_Messenger.png",
+    icon: "/logo/HOA_Messenger.png",
     sceenshots: [],
     url: "https://app.hoamessenger.com",
     repoType: "private",
@@ -13,7 +13,7 @@ const projects = [
     title: "RentScore",
     description:
       "Streamlines the rental property analysis process, empowering users to make well-informed decisions through a comprehensive rental analysis calculator.",
-    icon: "/Arunachalam_Portfolio/logo/RentScore.png",
+    icon: "/logo/RentScore.png",
     sceenshots: [],
     url: "https://rentscore.ai",
     repoType: "private",
@@ -23,7 +23,7 @@ const projects = [
     title: "Glintify",
     description:
       "A highly effective hyper-personalized client outreach strategy to significantly improve reply rates, meeting bookings, and sales within the realm of cold emails.",
-    icon: "/Arunachalam_Portfolio/logo/Glintify.png",
+    icon: "/logo/Glintify.png",
     sceenshots: [],
     url: "https://app.glintify.io",
     repoType: "private",
@@ -33,7 +33,7 @@ const projects = [
     title: "Resident Portal - HOA Messenger",
     description:
       "Residents of HOAs that are Premium Subscribers will be able to access their resident portals through the web dashboard.",
-    icon: "/Arunachalam_Portfolio/logo/HOA_Messenger.png",
+    icon: "/logo/HOA_Messenger.png",
     sceenshots: [],
     url: "https://resident.hoamessenger.com",
     repoType: "private",
@@ -43,7 +43,7 @@ const projects = [
     title: "Zen Print",
     description:
       "An innovative print automation solution designed to enhance the user's printing experience, allowing on-the-go document printing through collaboration with local vendors.",
-    icon: "/Arunachalam_Portfolio/logo/Zen_Print.png",
+    icon: "/logo/Zen_Print.png",
     sceenshots: [],
     url: "https://zen.gogosoon.com",
     repoType: "private",
@@ -61,7 +61,7 @@ const projects = [
     title: "Reed Exhibitions",
     description:
       "Operationalize the premium digital product hypothesis, following Data Mesh principles. Through adept storage, analysis, and processing, our team delivers impactful reports, enabling the client team to identify and enhance sales strategies.",
-    icon: "/Arunachalam_Portfolio/logo/Reed_Exhibitions.svg",
+    icon: "/logo/Reed_Exhibitions.svg",
     sceenshots: [],
     url: "https://rxglobal.com/",
     repoType: "private",
--- src/data/services.js
+++ src/data/services.js
@@ -1,25 +1,25 @@
 const services = [
   {
     title: "Mobile App Development",
-    image: "/Arunachalam_Portfolio/icons/mobile-dev.webp",
+    image: "/icons/mobile-dev.webp",
     description:
       "Specializing in React Native and Flutter, I excel in crafting high-performance mobile apps with seamless interfaces. My focus is on delivering robust functionality that aligns precisely with clients' business objectives. With expertise in ensuring intuitive user experiences, I bring a commitment to excellence in every project. ",
   },
   {
     title: "Web Development",
-    image: "/Arunachalam_Portfolio/icons/web-dev.webp",
+    image: "/icons/web-dev.webp",
     description:
       "I specialize in creating visually stunning, user-friendly websites that bolster online presence. From straightforward informational sites to intricate e-commerce platforms, I offer customized web development solutions. Leveraging the latest frameworks and technologies, I design responsive, SEO-friendly websites, ensuring a seamless browsing experience across all devices. My commitment lies in delivering web solutions that align with clients' unique needs and goals. ",
   },
   {
     title: "Backend Development",
-    image: "/Arunachalam_Portfolio/icons/backend-dev.webp",
+    image: "/icons/backend-dev.webp",
     description:
       "Specializing in backend development, I strengthen digital applications with a robust and scalable infrastructure. My expertise includes crafting efficient database structures, developing APIs, and configuring servers for optimal performance and security. I empower applications to seamlessly manage high traffic and complex data, ensuring they operate with efficiency and scalability. ",
   },
   {
     title: "DevOps",
-    image: "/Arunachalam_Portfolio/icons/backend-dev.webp",
+    image: "/icons/backend-dev.webp",
     description:
       "With a robust background in DevOps engineering, I bring extensive experience in configuring CI/CD pipelines. I have successfully implemented these pipelines for enterprise-level projects, utilizing the power of AWS Cloud and Bamboo. My expertise lies in streamlining development workflows, enhancing deployment processes, and ensuring seamless integration for efficient and scalable software delivery. ",
   },
